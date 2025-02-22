505cef88155ad8da488790112af235e3a0da6246
QuangHoan3112 <quanghoan10930@gmail.com>
2025-02-22 11:26:34 +0700

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static String printHello() {
+        return "HelloWord";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+}
